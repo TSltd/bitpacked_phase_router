b600aa07b92b92754ad593b2f286827eda81f904
TSltd <danvso@hotmail.com>
2026-03-12 22:02:31 +0000

    pyproject.toml

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ import numpy
 
 ext = Extension(
     "router",
-    sources=["src/router.cpp"],  
+    sources=["src/router.cpp"],
     include_dirs=[
         pybind11.get_include(),
         numpy.get_include()
     ],
-    extra_compile_args=["-O3", "-fopenmp", "-std=c++17"], 
+    extra_compile_args=["-O3", "-fopenmp", "-std=c++17"],
     extra_link_args=["-fopenmp"],
     language="c++"
 )
 
 setup(
-    name="router",
+    name="bitpacked_phase_router",
+    version="0.1.0",
+    packages=["src"],
+    package_dir={"": "."},
+    py_modules=["router_py"],
     ext_modules=[ext],
 )
